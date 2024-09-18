ac0e8b79f9195da3f261e47afc1ec2e510deba78
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-17 19:37:19 -0500

    feat(home): add home component.

---
+++ src/public/pages/home.component.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "home"
+}
+</script>
+
+<template>
+  <div class="w-full">
+    <h1>Home</h1>
+    <p>Welcome to ACME Learning Center.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
