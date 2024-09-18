32917c82da53c6acfa245476efbb7a65b585f258
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-17 19:43:47 -0500

    feat(in-app-navigation): add in-app navigation rules to router.

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import {createRouter, createWebHistory} from "vue-router";
+import HomeComponent from "../public/pages/home.component.vue";
+import AboutComponent from "../public/pages/about.component.vue";
+import CategoryManagementComponent from "../publishing/pages/category-management.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: []
+    routes: [
+        { path: '/home',                    name: 'home', component: HomeComponent, meta: { title: 'Home'}},
+        { path: '/about',                   name: 'about', component: AboutComponent, meta: { title: 'About Us'}},
+        { path: '/publishing/categories',   name: 'categories', component: CategoryManagementComponent, meta: { title: 'Category Management'}},
+        { path: '/',                        redirect: '/home'}
+    ]
 });
 
+router.beforeEach((to, from, next) => {
+    let baseTitle = 'ACME Learning Center';
+    document.title = `${baseTitle} | ${to.meta['title']}`;
+    next();
+})
 export default router;
