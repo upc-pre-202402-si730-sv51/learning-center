9426ba7bec8d76c3be775205a860bf934d024c0b
Angel Velasquez <pcisavel@upc.edu.pe>
2024-09-17 19:53:51 -0500

    feat(main): implement main view.

---
+++ src/app.vue
@@ -0,0 +1,55 @@
+<script>
+import LanguageSwitcher from "./public/components/language-switcher.component.vue";
+
+export default {
+  name: 'app',
+  components: {LanguageSwitcher},
+  title: 'ACME Learning Center',
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { label: 'Home', to: '/home'},
+        { label: 'About', to: '/about'},
+        { label: 'Categories', to: '/publishing/categories'}
+      ]
+    }
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    }
+  }
+}
+
+</script>
+
+<template>
+  <pv-toast/>
+  <header>
+    <pv-toolbar class="bg-primary">
+      <template #start>
+        <pv-button class="p-button-text" icon="pi pi-bars" @click="toggleDrawer"/>
+        <h2>ACME Learning Center</h2>
+      </template>
+      <template #center>
+        <div class="flex-column">
+          <router-link v-for="item in items" :key="item.label" v-slot="{navigate, href}" :to="item.to" custom>
+            <pv-button :href="href" class="p-button-text" @click="navigate">{{ item.label }}</pv-button>
+          </router-link>
+        </div>
+      </template>
+      <template #end>
+        <language-switcher/>
+      </template>
+    </pv-toolbar>
+    <pv-drawer v-model:visible="drawer"/>
+  </header>
+  <main>
+    <router-view/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import {createApp} from 'vue'
 import './style.css'
-import App from './App.vue'
+import App from './app.vue'
 import i18n from "./i18n.js";
 
 // PrimeVue
